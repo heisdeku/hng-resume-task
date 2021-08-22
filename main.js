3c1dd9448b860ac5e20988427f8d210ba726e8a8
Deku || Feranmi <adedokunoluwaferanmi@gmail.com>
2021-08-22 22:16:52 +0100

    files uploaded in time

---
+++ main.js
@@ -0,0 +1,5 @@
+const myName = (first_name, last_name) => {
+    return `My name is ${first_name}, ${last_name}`;
+};
+
+console.log(myName('Oluwaferanmi', 'Adedokun')); // My name is Oluwaferanmi, Adedokun
